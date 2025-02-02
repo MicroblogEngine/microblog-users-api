2dfe993b09c6c5fb4229845deec4688e3716ad7c
Rogerio Pereira Araujo <rogerio.araujo@gmail.com>
2025-02-02 09:51:11 -0300

    [fix[ improving implementation of grpc server

--- apps/worker-grpc/src/index.ts
+++ apps/worker-grpc/src/index.ts
@@ -1,8 +1,32 @@
-import { Topics } from "@ararog/microblog-server";
+import {createServer, CallContext} from 'nice-grpc';
+import { prisma } from "@ararog/microblog-users-api-db";
+import {
+  DeepPartial, 
+  GetUsersRequest,
+  GetUsersResponse,
+  UsersServiceDefinition, 
+  UsersServiceImplementation 
+} from '@ararog/microblog-rpc';
+
+const usersServiceImpl: UsersServiceImplementation = {
+  async getUsers(
+    request: GetUsersRequest,
+    context: CallContext
+  ): Promise<DeepPartial<GetUsersResponse>> {
+    const users = await prisma.user.findMany();
+    return { users };
+  }
+};
+
 
 const startServer = async () => {
-  console.info('Starting server');
-  //
+  console.log('Starting server');
+  const server = createServer();
+
+  server.add(UsersServiceDefinition, usersServiceImpl);
+
+  console.log('Server listening on', process.env.GRPC_HOST || '0.0.0.0:8080');
+  await server.listen(process.env.GRPC_HOST || '0.0.0.0:8080');
 }
 
 startServer();
